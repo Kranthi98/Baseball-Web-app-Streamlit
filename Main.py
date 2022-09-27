d32d3111d7d225246b9ca346709598608040742d
Jakkoju Kranthi Kumar <61588620+Kranthi98@users.noreply.github.com>
2022-09-27 12:18:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -68,7 +68,7 @@ with col1:
     st.subheader(f"Pitching stats for {pitcher1}")
 
     #if fg == "Pitch_Splits":
-    pitcher1_df = (pb.statcast_pitcher(player_id = pb.playerid_lookup(name_split(pitcher1)[1],name_split(pitcher1)[0])["key_mlbam"].values[0],
+    pitcher1_df = (pb.statcast_pitcher(player_id = pb.playerid_lookup(name_split(pitcher1)[1].strip(),name_split(pitcher1)[0])["key_mlbam"].values[0],
                 start_dt = start_date, end_dt = end_date)
                 .assign(pitch_type = lambda x : x.pitch_type.replace(pt_dict),
                 events = lambda x : x.events.fillna("None")))
@@ -83,7 +83,7 @@ with col1:
 with col2 : 
     st.subheader(f"Pitching stats for {pitcher2}")
     #if fg == "Pitch_Splits":
-    pitcher2_df = (pb.statcast_pitcher(player_id = pb.playerid_lookup(name_split(pitcher2)[1],name_split(pitcher2)[0])["key_mlbam"].values[0],
+    pitcher2_df = (pb.statcast_pitcher(player_id = pb.playerid_lookup(name_split(pitcher2)[1].strip(),name_split(pitcher2)[0])["key_mlbam"].values[0],
                 start_dt = start_date, end_dt = end_date)
                 .assign(pitch_type = lambda x : x.pitch_type.replace(pt_dict),
                 events = lambda x : x.events.fillna("None")))
